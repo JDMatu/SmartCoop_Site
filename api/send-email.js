10696b02751b4aa3b3e9dc7d308411386da4607f
MatuDev <juandiegomatute11@gmail.com>
2024-11-14 22:00:25 -0600

    New Release

--- api/send-email.js
+++ api/send-email.js
@@ -1,5 +1,5 @@
 import { Resend } from 'resend';
-
+import { generateDemoEmailTemplate } from './email.js';
 // Instancia de Resend con tu API Key
 const resend = new Resend(process.env.RESEND_API_KEY); 
 
@@ -8,18 +8,39 @@ export default async function (req, res) {
     return res.status(405).json({ error: 'Método no permitido, use POST' });
   }
 
-  const { name, email, message } = req.body;
+  const { name, email, asunto, message, from } = req.body;
 
   try {
-    // Envía el correo utilizando Resend
-    await resend.emails.send({
-      from: 'contacto@smartcoopcr.com',  // Usa un correo autorizado en Resend
-      to: email,
-      subject: `Nuevo mensaje de ${name}`,
-      text: `Detalles:\nNombre: ${name}\nEmail: ${email}\nMensaje: ${message}`,
-    });
+    if (from === 'demo'){
+      const demoEmailHtml = generateDemoEmailTemplate(name);
+      // Envía el correo utilizando Resend
+      await resend.batch.send([
+        {
+        from: 'info@smartcoopcr.com',  // Usa un correo autorizado en Resend
+        to: 'contacto@smartcoopcr.com',
+        subject: `${asunto} - ${name}`,
+        text: `Detalles:\nNombre: ${name}\nEmail: ${email}\nMensaje: ${message}`,
+      },
+      {
+        from: 'contacto@smartcoopcr.com',
+        to: email,
+        subject: 'Demo SmartCoop',
+        html: demoEmailHtml,
+      },
+      ]);
 
-    return res.status(200).json({ message: 'Correo enviado exitosamente' });
+      return res.status(200).json({ message: 'Correo enviado exitosamente' });
+    }else{
+      // Envía el correo utilizando Resend
+      await resend.emails.send({
+        from: 'info@smartcoopcr.com',  // Usa un correo autorizado en Resend
+        to: 'contacto@smartcoopcr.com',
+        subject: `Mensaje de contacto - ${asunto}`,
+        text: `Detalles:\nNombre: ${name}\nEmail: ${email}\nMensaje: ${message}`,
+      });
+
+      return res.status(200).json({ message: 'Correo enviado exitosamente' });
+    }
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: 'Error al enviar el correo' });
